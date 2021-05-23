93a10089eebd4f5c8f623b176fa873b94842ea76
sinanclt <sinanahammed313@gmail.com>
2021-05-23 22:47:49 +0530

    hlo

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jo^zsg%^2=geclqa^g^sn!m8gk(m_ikw8c@%t%$^%h!2em-*+i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
